65d325bf9bc6804fbabde4fc69ae3694f0492d1d
Veta Sheina <sssheina@gmail.com>
2023-01-07 17:24:59 +0300

    jsKate

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,9 +1,22 @@
+
 const inbox = document.querySelector(".inbox__input");
 const inboxButton = document.querySelector(".inbox__button");
 const placeInboxList = document.querySelector(".inbox__listItems");
 const buttonInboxEdit = document.querySelector(".inbox__btn-edit");
 const buttonInboxDelite = document.querySelector(".inbox__btn-delite");
 let inboxItem = document.querySelector(".inbox__item");
+
+// let nameTask == inbox.value;
+
+let task = {
+    nameTask: "",
+    category: "",
+    context: "",
+    stages:[]
+
+}
+
+
 
 let arrayInbox = [];
 
@@ -12,7 +25,7 @@ inboxButton.addEventListener("click", generateInboxList);
 function generateInboxList() {
     arrayInbox.push(inbox.value);
     console.log(arrayInbox);
-    placeInboxList.innerHTML += `<div class="inbox__listItem listItem">
+    placeInboxList.innerHTML += `<li class="inbox__listItem listItem">
                       
 
     <div class="inbox__inputfield">
@@ -39,10 +52,12 @@ function generateInboxList() {
         alt="logo_white"
       />
     </button>
-  </div>`
+  </li>`
 
   inbox.value = "";
+  console.log(buttonInboxEdit);
 }
+
 
 function InboxDelite() {
     inboxItem.innerHTML = null;
@@ -57,68 +72,68 @@ function InboxDelite() {
 
 // ЧЕК_ЛИСТ
 
-$('.task__add').on('focus',function(){
-  $(this).val('');
-});
+// $('.task__add').on('focus',function(){
+//   $(this).val('');
+// });
 
-$('.task__add').on('blur',function(){
-  $(this).val('+ add new task');
-});
+// $('.task__add').on('blur',function(){
+//   $(this).val('+ add new task');
+// });
 
-$('form').on('submit', function(event){
-  event.preventDefault();
+// $('form').on('submit', function(event){
+//   event.preventDefault();
   
-  var taskText = $('.task__add').val();
-  var tasksN = $('.task').length + 1;
+//   var taskText = $('.task__add').val();
+//   var tasksN = $('.task').length + 1;
   
-  var newTask = '<label for="task--' + tasksN + '" class="task task--new"><input class="task__check" type="checkbox" id="task--' + tasksN + '" /> <div class="task__field task--row">' + taskText + '<button class="task__important"><i class="fa fa-check" aria-hidden="true"></i></button></div></label>'
+//   var newTask = '<label for="task--' + tasksN + '" class="task task--new"><input class="task__check" type="checkbox" id="task--' + tasksN + '" /> <div class="task__field task--row">' + taskText + '<button class="task__important"><i class="fa fa-check" aria-hidden="true"></i></button></div></label>'
 
   
-  $('.task__list').append(newTask);
+//   $('.task__list').append(newTask);
 
-  $('.task__add').val('');
-  checkList();
-});
+//   $('.task__add').val('');
+//   checkList();
+// });
 
-var lastDeletedTask = '';
+// var lastDeletedTask = '';
 
 
-function checkList() {
+// function checkList() {
   
   
-  $('.task').each(function(){
+//   $('.task').each(function(){
 
-    var $field = $(this).find('.task__field');
-    var mousedown = false;
+//     var $field = $(this).find('.task__field');
+//     var mousedown = false;
 
 
-    $field.on('mousedown', function(){
-        mousedown = true;
-        $field.addClass('shaking');
-        setTimeout(deleteTask,1000)
-    });
+//     $field.on('mousedown', function(){
+//         mousedown = true;
+//         $field.addClass('shaking');
+//         setTimeout(deleteTask,1000)
+//     });
 
-    $field.on('mouseup', function(){
-        mousedown = false;
-        $field.removeClass('shaking');
-    });
+//     $field.on('mouseup', function(){
+//         mousedown = false;
+//         $field.removeClass('shaking');
+//     });
 
-    function deleteTask(){
-      if(mousedown) {
-        $field.addClass('delete');
-        lastDeletedTask = $field.text();
-        console.log(lastDeletedTask);
+//     function deleteTask(){
+//       if(mousedown) {
+//         $field.addClass('delete');
+//         lastDeletedTask = $field.text();
+//         console.log(lastDeletedTask);
         
-        setTimeout(function(){
-           $field.remove();
-        }, 200);
-       } else {return;}
-    }
+//         setTimeout(function(){
+//            $field.remove();
+//         }, 200);
+//        } else {return;}
+//     }
 
-  });
-}
+//   });
+// }
 
-checkList();
+// checkList();
 
 
 
