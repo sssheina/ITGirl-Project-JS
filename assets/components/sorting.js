5ef86952d82d729d6468db06ff8509df191cc94e
lisa_ywl <yourworthlesslife@icloud.com>
2023-01-18 20:11:49 +0300

    context sorting

---
+++ assets/components/sorting.js
@@ -0,0 +1,77 @@
+addEventListener('DOMContentLoaded', () => {
+  const arrTasks = getLocStorage("editedTasks");
+
+  if (window.location.href.split('/').at(-1) == "10_category_sorting.html?category-home") {
+    sortByCategory(arrTasks, 'Дом');
+  }
+
+  else if (window.location.href.split('/').at(-1) == "10_category_sorting.html?category-study") {
+    sortByCategory(arrTasks, 'Учеба');
+  }
+  else if (window.location.href.split('/').at(-1) == "10_category_sorting.html?category-work") {
+    sortByCategory(arrTasks, 'Работа');
+  }
+  else if (window.location.href.split('/').at(-1) == "10_category_sorting.html?category-selfimprovment") {
+    sortByCategory(arrTasks, 'Саморазвитие');
+  }
+  else if (window.location.href.split('/').at(-1) == "10_category_sorting.html?category-health") {
+    sortByCategory(arrTasks, 'Здоровье');
+  }
+  else if (window.location.href.split('/').at(-1) == "10_category_sorting.html?category-rest") {
+    sortByCategory(arrTasks, 'Отдых');
+  }
+  else if (window.location.href.split('/').at(-1) == "10_category_sorting.html?category-hobby") {
+    sortByCategory(arrTasks, 'Хобби');
+  }
+  else if (window.location.href.split('/').at(-1) == "10_category_sorting.html?category-ideas") {
+    sortByCategory(arrTasks, 'Идеи');
+  }
+  else if (window.location.href.split('/').at(-1) == "10_category_sorting.html?category-household") {
+    sortByCategory(arrTasks, 'Хозяйство');
+  }
+  else if (window.location.href.split('/').at(-1) == "10_category_sorting.html?category-sport") {
+    sortByCategory(arrTasks, 'Спорт');
+  }
+  else if (window.location.href.split('/').at(-1) == "10_category_sorting.html?category-selfcare") {
+    sortByCategory(arrTasks, 'Уход за собой');
+  }
+
+  if (window.location.href.split('/').at(-1) == "12_context_sorting.html?context-tel") {
+    sortByContext(arrTasks, 'Телефон');
+  }
+  else if (window.location.href.split('/').at(-1) == "12_context_sorting.html?context-home") {
+    sortByContext(arrTasks, 'Дом');
+  }
+  else if (window.location.href.split('/').at(-1) == "12_context_sorting.html?context-outside") {
+    sortByContext(arrTasks, 'Вне дома');
+  }
+  else if (window.location.href.split('/').at(-1) == "12_context_sorting.html?context-school") {
+    sortByContext(arrTasks, 'Школа');
+  }
+  else if (window.location.href.split('/').at(-1) == "12_context_sorting.html?context-kindergarden") {
+    sortByContext(arrTasks, 'Детсад');
+  }
+  else if (window.location.href.split('/').at(-1) == "12_context_sorting.html?context-computer") {
+    sortByContext(arrTasks, 'Компьютер');
+  }
+  else if (window.location.href.split('/').at(-1) == "12_context_sorting.html?context-minsk") {
+    sortByContext(arrTasks, 'Минск');
+  }
+  else if (window.location.href.split('/').at(-1) == "12_context_sorting.html?context-in-town") {
+    sortByContext(arrTasks, 'Город');
+  }
+  else if (window.location.href.split('/').at(-1) == "12_context_sorting.html?context-delegated") {
+    sortByContext(arrTasks, 'Делегироване');
+  }
+
+});
+
+function sortByCategory(tasks, category) {
+  const arr = tasks.filter(el => el.category === `${category}`);
+  insertTasks(arr);
+}
+
+function sortByContext(tasks, context) {
+  const arr = tasks.filter(el => el.context === `${context}`);
+  insertTasks(arr);
+}
