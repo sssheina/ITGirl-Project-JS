2139cfca84f73487a55bc23746ce0d5b8ccb1f65
Veta Sheina <sssheina@gmail.com>
2023-01-18 22:20:14 +0300

    checklist

--- index.js
+++ index.js
@@ -552,7 +552,7 @@ let inp = document.querySelector(".todo__inp");
 
     btnAdd.onclick = function() {
       createTodo();
-      inp = "";
+      
     }
 
     list.addEventListener("click", function(e) {
