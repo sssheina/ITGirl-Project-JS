18c3fa201fdb20bbb93825dba6804f71479850b0
Ольга Малышева <olga.maysheva.smm@gmail.com>
2023-01-20 15:24:32 +0200

    нельзя добавить пустой входящий

---
+++ assets/components/reference.js
@@ -0,0 +1,7 @@
+function validateModalWindow(arrayReference) {
+    arrayReference.forEach(el => {
+        if (el.type.value === 'Справочные материалы') {
+            type.innerHTML = 'Справочные материалы';
+          }
+})
+};
